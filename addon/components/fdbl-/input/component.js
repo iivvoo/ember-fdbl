477368c266fed9009cc1abf308a1068003a1cda0
Stephan <stephan_stolk@hotmail.com>
2016-06-30 15:38:55 +0200

    Added option to have input label on the right side

--- addon/components/fdbl-/input/component.js
+++ addon/components/fdbl-/input/component.js
@@ -1,7 +1,15 @@
+import Ember from 'ember';
 import BaseComponent from '../base';
 import layout from './template';
 
 export default BaseComponent.extend({
     size: 10,
-    layout: layout
+    layout: layout,
+    labelPostition: Ember.computed('label', 'position', function() {
+      if(this.get('label') && this.get('position') === 'right') {
+        return 'right';
+      } else if(this.get('label')) {
+        return 'left';        
+      }
+    })
 });
