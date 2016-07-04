0af4007bc21efd74d9411b7b00a80b7bbd21cbeb
Stephan <stephan_stolk@hotmail.com>
2016-07-04 11:02:46 +0200

    Created integration test for new labelposition parameter that adds an option to display labels of input fields on the right side of an input field

--- tests/integration/components/fdbl-/input/component-test.js
+++ tests/integration/components/fdbl-/input/component-test.js
@@ -1,24 +1,80 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('fdbl-/input', 'Integration | Component | fdbl /input', {
-  integration: true
+const model = Ember.Object.create({
+  id: 1,
+  name: ''
 });
 
-test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+const eq = Ember.Helper.helper(function(params) {
+  console.log('param 1 ' + params[0] + ' ' + params[1]);
+  if(params[0] === params[1]) {
+    return true;
+  }
+  return false;
+});
 
-  this.render(hbs`{{fdbl-/input}}`);
+moduleForComponent('fdbl-/input', 'Integration | Component | fdbl /input', {
+  integration: true,
+  beforeEach() {
+    this.register('helper:eq', eq);
 
-  assert.equal(this.$().text().trim(), '');
+    this.set('model', model);
+    this.set('model.name', 'John');
+    this.set('property', 'name');
+    this.set('label', 'name');
+  }
+});
 
-  // Template block usage:
-  this.render(hbs`
-    {{#fdbl-/input}}
-      template block text
-    {{/fdbl-/input}}
-  `);
+test('No labelPosition', function(assert) {
+  /**
+   * When no parameter called labelPostion is given but one called
+   * label is a label should appear on the left.
+   */
 
-  assert.equal(this.$().text().trim(), 'template block text');
+  this.render(hbs`{{fdbl-/input model=model
+                                property=property
+                                label=label}}`);
+
+  assert.equal(this.$('label').length, 1);
+
+  assert.equal(this.$('div.form-group >:first-child').is('label'), true);
+});
+
+test('LabelPosition is left', function(assert) {
+  /**
+   * When a parameter called labelPostion with a value of 'left' is given and
+   * one called label is also given a label should appear on the left.
+   */
+
+  this.set('labelPostition', 'left');
+
+  this.render(hbs`{{fdbl-/input model=model
+                                property=property
+                                label=label
+                                labelPostition=labelPostition}}`);
+
+  assert.equal(this.$('label').length, 1);
+
+  assert.equal(this.$('div.form-group >:first-child').is('label'), true);
+});
+
+test('LabelPosition is right', function(assert) {
+  /**
+   * When a parameter called labelPostion with a value of 'right' is given and
+   * one called label is also given a label should appear on the right.
+   */
+
+  this.set('labelPostition', 'right');
+
+  this.render(hbs`{{fdbl-/input model=model
+                                property=property
+                                label=label
+                                labelPostition=labelPostition}}`);
+
+
+  assert.equal(this.$('label').length, 1);
+
+  assert.equal(this.$('div.form-group >:last-child').is('label'), true);
 });
