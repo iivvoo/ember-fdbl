a6966f203102afb2277f530cab35a0ba7f7599a9
Stephan <stephan_stolk@hotmail.com>
2016-08-29 15:50:15 +0200

    Fixed typo

--- addon/components/fdbl-/input/component.js
+++ addon/components/fdbl-/input/component.js
@@ -5,7 +5,7 @@ import layout from './template';
 export default BaseComponent.extend({
     size: 10,
     layout: layout,
-    labelPostition: Ember.computed('label', 'position', function() {
+    labelPosition: Ember.computed('label', 'position', function() {
       if(this.get('label') && this.get('position') === 'right') {
         return 'right';
       } else if(this.get('label')) {
